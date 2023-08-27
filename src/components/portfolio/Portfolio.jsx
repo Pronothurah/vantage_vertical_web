ea70463c718e45b1d2dfc4bdf6917e87767797bd
Pronothurah <onsongoonditi@gmail.com>
2023-08-27 15:18:00 +0300

    portfolio partners

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 
-import './portfolio.css'
+import { motion } from 'framer-motion';
+
+import './portfolio.css';
+
+import dji from '../../assets/dji.jpg';
+import kolida from '../../assets/kolida.jpg'
+import baofeng from '../../assets/baofeng.jpg'
 
 const Portfolio = () => {
   return (
     <div>
       <div className='vantage__portfolio'>
             <div className="vantage__portfolio-heading section__padding gradient__bg">  
-                    <h1>PORTFOLIO</h1>
-                    <p>UNLOCKING BOUNDLESS CREATIVITY</p>        
+              <h1>PORTFOLIO</h1>
+              <p>UNLOCKING BOUNDLESS CREATIVITY</p>
             </div>
+            <motion.div className='vantage__portfolio-partners section__padding gradient__bg'>
+                <div className='vantage__portfolio-partner'>
+                      <img src={dji} alt="dji"/>
+                </div>
+                <div className='vantage__portfolio-partner'>
+                      <img src={baofeng} alt="baofeng"/>
+                </div>
+                <div className='vantage__portfolio-partner'>
+                      <img src={kolida} alt="kolida"/>
+                </div>
+              </motion.div>
         </div>
     </div>
   )
